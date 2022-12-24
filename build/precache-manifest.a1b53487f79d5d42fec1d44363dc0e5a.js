b209a3461b282fc11c4f98f156fbfbc15a30eeb6
jonatanzafar59 <jonatanzafar59@gmail.com>
2022-12-24 01:45:03 +0100

    service Catalog App skelaton

---
+++ build/precache-manifest.a1b53487f79d5d42fec1d44363dc0e5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e144bc4e6599da0c3486db714b0a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4caea23cf03c201cda3",
+    "url": "/static/css/main.69350da1.chunk.css"
+  },
+  {
+    "revision": "abbdec09d7aab285e6e5",
+    "url": "/static/js/2.20b4f4f4.chunk.js"
+  },
+  {
+    "revision": "a4caea23cf03c201cda3",
+    "url": "/static/js/main.400a8511.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
